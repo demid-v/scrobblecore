395ae2fa00533cc85457047abd4150da4bd7ea3c
Demid <vasilyev.demid.e@gmail.com>
2024-11-12 10:44:25 +0300

    Install import sorting plugin

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,18 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+/**
+ * @typedef {import('prettier').Config} PrettierConfig
+ * @typedef {import('prettier-plugin-tailwindcss').PluginOptions} TailwindPluginOptions
+ * @typedef {import('@trivago/prettier-plugin-sort-imports').PluginConfig} SortImportsPluginConfig
+ *
+ * @type {PrettierConfig & TailwindPluginOptions & SortImportsPluginConfig}
+ */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: [
+    "@trivago/prettier-plugin-sort-imports",
+    // Must come last. https://dev.to/kachidk/common-prettier-plugins-installation-30hc
+    "prettier-plugin-tailwindcss",
+  ],
+  importOrder: [],
+  importOrderSortSpecifiers: true,
 };
 
 export default config;
