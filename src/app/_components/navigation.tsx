b7db46334c77f53bc7f9e0dff29137d946358ce1
Demid <vasilyev.demid.e@gmail.com>
2024-12-04 10:51:26 +0300

    Add active state to navigation

--- src/app/_components/navigation.tsx
+++ src/app/_components/navigation.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import * as React from "react";
 
 import {
@@ -11,54 +12,77 @@ import {
   navigationMenuTriggerStyle,
 } from "~/components/ui/navigation-menu";
 
-const Navigation = ({ className }: { className?: string }) => (
-  <NavigationMenu className={className}>
-    <NavigationMenuList>
-      <NavigationMenuItem>
-        <Link href="/search" legacyBehavior passHref>
-          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-            Search
-          </NavigationMenuLink>
-        </Link>
-      </NavigationMenuItem>
-    </NavigationMenuList>
-    <NavigationMenuList>
-      <NavigationMenuItem>
-        <Link href="/albums" legacyBehavior passHref>
-          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-            Albums
-          </NavigationMenuLink>
-        </Link>
-      </NavigationMenuItem>
-    </NavigationMenuList>
-    <NavigationMenuList>
-      <NavigationMenuItem>
-        <Link href="/tracks" legacyBehavior passHref>
-          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-            Tracks
-          </NavigationMenuLink>
-        </Link>
-      </NavigationMenuItem>
-    </NavigationMenuList>
-    <NavigationMenuList>
-      <NavigationMenuItem>
-        <Link href="/artists" legacyBehavior passHref>
-          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-            Artists
-          </NavigationMenuLink>
-        </Link>
-      </NavigationMenuItem>
-    </NavigationMenuList>
-    <NavigationMenuList>
-      <NavigationMenuItem>
-        <Link href="/track" legacyBehavior passHref>
-          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-            Track
-          </NavigationMenuLink>
-        </Link>
-      </NavigationMenuItem>
-    </NavigationMenuList>
-  </NavigationMenu>
-);
+const Navigation = ({ className }: { className?: string }) => {
+  const pathname = usePathname().split("/");
+  const firstPathname = pathname.at(1);
+
+  const isActive = (match: string) =>
+    match === firstPathname && pathname.length === 2;
+
+  return (
+    <NavigationMenu className={className}>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/search" legacyBehavior passHref>
+            <NavigationMenuLink
+              className={navigationMenuTriggerStyle()}
+              active={isActive("search")}
+            >
+              Search
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/albums" legacyBehavior passHref>
+            <NavigationMenuLink
+              className={navigationMenuTriggerStyle()}
+              active={isActive("albums")}
+            >
+              Albums
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/tracks" legacyBehavior passHref>
+            <NavigationMenuLink
+              className={navigationMenuTriggerStyle()}
+              active={isActive("tracks")}
+            >
+              Tracks
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/artists" legacyBehavior passHref>
+            <NavigationMenuLink
+              className={navigationMenuTriggerStyle()}
+              active={isActive("artists")}
+            >
+              Artists
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/track" legacyBehavior passHref>
+            <NavigationMenuLink
+              className={navigationMenuTriggerStyle()}
+              active={isActive("track")}
+            >
+              Track
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
 
 export default Navigation;
